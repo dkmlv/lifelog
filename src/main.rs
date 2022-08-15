aa0e2b86d0d7b20dbd985e184f5a022ef3a22344
DurbeKK <k2.durbek@gmail.com>
2022-08-15 17:46:58 +0500

    add a basic main menu
    none of the buttons work except 'quit'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use cursive::align::HAlign;
+use cursive::views::{Dialog, DialogFocus};
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = cursive::default();
+
+    let mut dialog = Dialog::text("welcome to lifelog, a log of your uneventful life.")
+        .button("entries", |_s| {})
+        .button("new entry", |_s| {})
+        .button("about", |_s| {})
+        .button("quit", |s| s.quit())
+        .h_align(HAlign::Center);
+
+    // focus on 'new entry' button
+    dialog.set_focus(DialogFocus::Button(1));
+
+    siv.add_layer(dialog.title("lifelog."));
+
+    siv.run();
 }
